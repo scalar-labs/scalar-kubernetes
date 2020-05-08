35a674cbc080b42af27cc9b04b85fc7835fce940
Paul D <paul.dozancuk@scalar-labs.com>
2020-05-08 17:34:37 +0900

    remove network modules

--- modules/azure/network/locals.tf
+++
@@ -1,9 +0,0 @@
-
-locals {
-  subnet = {
-    public       = cidrsubnet(local.network.cidr, 6, 0)
-    k8s_node_pod = cidrsubnet(local.network.cidr, 6, 1)
-    cassandra    = cidrsubnet(local.network.cidr, 6, 2)
-    k8s_ingress  = cidrsubnet(local.network.cidr, 6, 3)
-  }
-}
