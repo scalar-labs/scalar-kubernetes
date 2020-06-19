419694aa16e1f0c24221194f31c2c2afb9df884e
Paul D <paul.dozancuk@scalar-labs.com>
2020-06-19 17:15:19 +0900

    remove hardcode and use regex

--- modules/azure/kubernetes/output.tf
+++ modules/azure/kubernetes/output.tf
@@ -19,7 +19,7 @@ Host *.${var.network.internal_domain}
   User ${var.network.user_name}
   ProxyCommand ssh -F ssh.cfg bastion -W %h:%p
 
-Host 10.*
+Host ${regex("(^10\\.|^172\\.1[6-9]\\.|^172\\.2[0-9]\\.|^172\\.3[0-1]\\.|^192\\.168\\.)", var.network.cidr)[0]}*
   User ${local.kubernetes_cluster.admin_username}
   ProxyCommand ssh -F ssh.cfg bastion -W %h:%p
 EOF
