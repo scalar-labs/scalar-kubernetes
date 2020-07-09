98d092d9500ba1dbac15876cd35df97dc515d2c8
Paul D <paul.dozancuk@scalar-labs.com>
2020-07-09 16:01:40 +0900

    use v1.3.0

--- test/modules/azuredeploy/cassandra/main.tf
+++ test/modules/azuredeploy/cassandra/main.tf
@@ -1,5 +1,5 @@
 module "cassandra" {
-  source = "git::https://github.com/scalar-labs/scalar-terraform.git//modules/azure/cassandra?ref=master"
+  source = "git::https://github.com/scalar-labs/scalar-terraform.git//modules/azure/cassandra?ref=v1.3.0"
 
   # Required Variables (Use network remote state)
   network = local.network
--- test/modules/azuredeploy/monitor/main.tf
+++ test/modules/azuredeploy/monitor/main.tf
@@ -1,5 +1,5 @@
 module "monitor" {
-  source = "git::https://github.com/scalar-labs/scalar-terraform.git//modules/azure/monitor?ref=master"
+  source = "git::https://github.com/scalar-labs/scalar-terraform.git//modules/azure/monitor?ref=v1.3.0"
 
   # Required Variables (Use remote state)
   network   = local.network
