d0836961780b7d22a2e04a1da005e551e2fa9a7a
Yusuke Morimoto <yusuke.morimoto@gmail.com>
2020-09-17 17:22:49 +0900

    Output only needed configurations from cosmosdb

--- modules/azure/cosmosdb/output.tf
+++ modules/azure/cosmosdb/output.tf
@@ -1,17 +1,5 @@
-output "cosmosdb_account_id" {
-  value = module.cosmosdb.cosmosdb_account_id
-}
-
 output "cosmosdb_account_endpoint" {
   value = module.cosmosdb.cosmosdb_account_endpoint
-}
-
-output "cosmosdb_account_read_endpoints" {
-  value = module.cosmosdb.cosmosdb_account_read_endpoints
-}
-
-output "cosmosdb_account_write_endpoints" {
-  value = module.cosmosdb.cosmosdb_account_write_endpoints
 }
 
 output "cosmosdb_account_primary_master_key" {
@@ -20,16 +8,4 @@ output "cosmosdb_account_primary_master_key" {
 
 output "cosmosdb_account_secondary_master_key" {
   value = module.cosmosdb.cosmosdb_account_secondary_master_key
-}
-
-output "cosmosdb_account_primary_readonly_master_key" {
-  value = module.cosmosdb.cosmosdb_account_primary_readonly_master_key
-}
-
-output "cosmosdb_account_secondary_readonly_master_key" {
-  value = module.cosmosdb.cosmosdb_account_secondary_readonly_master_key
-}
-
-output "cosmosdb_account_connection_strings" {
-  value = module.cosmosdb.cosmosdb_account_connection_strings
 }
