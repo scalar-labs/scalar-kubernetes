1f85c6faefc4bbe2cdea7cd417936f888afa9030
Yusuke Morimoto <yusuke.morimoto@gmail.com>
2020-09-29 16:00:53 +0900

    Remove unecessary keys from local.network

--- modules/azure/cosmosdb/locals.tf
+++ modules/azure/cosmosdb/locals.tf
@@ -1,15 +1,7 @@
 locals {
   network = {
-    cidr   = data.terraform_remote_state.network.outputs.network_cidr
     name   = data.terraform_remote_state.network.outputs.network_name
-    dns    = data.terraform_remote_state.network.outputs.dns_zone_id
-    id     = data.terraform_remote_state.network.outputs.network_id
     region = data.terraform_remote_state.network.outputs.region
-
-    bastion_ip           = data.terraform_remote_state.network.outputs.bastion_ip
-    bastion_provision_id = data.terraform_remote_state.network.outputs.bastion_provision_id
-
-    internal_domain = data.terraform_remote_state.network.outputs.internal_domain
   }
 
   kubernetes = {
