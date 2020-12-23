ee3c1abf448140015d0358a9bcc77317ac0aee2d
Yusuke Morimoto <yusuke.morimoto@scalar-labs.com>
2020-12-24 00:40:56 +0900

    Change argument for scalar-terraform cosmosdb module

--- modules/azure/cosmosdb/main.tf
+++ modules/azure/cosmosdb/main.tf
@@ -2,6 +2,6 @@ module "cosmosdb" {
   source = "git::https://github.com/scalar-labs/scalar-terraform.git//modules/azure/cosmosdb?ref=master"
 
   # Required Variables
-  network    = local.network
-  kubernetes = local.kubernetes
+  network            = local.network
+  allowed_subnet_ids = [local.kubernetes.node_pool_subnet_id]
 }
